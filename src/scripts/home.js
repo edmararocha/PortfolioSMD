37cafc54658c7cbb3ec9ed3210d0b076c1626754
Israel Leite <80715640+isradesu@users.noreply.github.com>
2023-06-25 21:42:30 -0300

    adicionando animação das nuvens

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -71,3 +71,96 @@ dog.addEventListener('mouseout', function() {
 
 // });}
 
+var imagePaths = [
+  '../src/images/home/cloud_frames/frame1.png',
+  '../src/images/home/cloud_frames/frame2.png',
+  '../src/images/home/cloud_frames/frame3.png',
+  '../src/images/home/cloud_frames/frame4.png',
+  '../src/images/home/cloud_frames/frame5.png',
+  '../src/images/home/cloud_frames/frame6.png',
+  '../src/images/home/cloud_frames/frame7.png',
+  '../src/images/home/cloud_frames/frame8.png',
+  '../src/images/home/cloud_frames/frame9.png',
+  '../src/images/home/cloud_frames/frame10.png',
+  '../src/images/home/cloud_frames/frame12.png',
+  '../src/images/home/cloud_frames/frame13.png',
+  '../src/images/home/cloud_frames/frame14.png',
+  '../src/images/home/cloud_frames/frame15.png',
+  '../src/images/home/cloud_frames/frame16.png',
+  '../src/images/home/cloud_frames/frame17.png',
+  '../src/images/home/cloud_frames/frame18.png',
+  '../src/images/home/cloud_frames/frame19.png',
+  '../src/images/home/cloud_frames/frame20.png',
+  '../src/images/home/cloud_frames/frame21.png',
+  '../src/images/home/cloud_frames/frame22.png',
+  '../src/images/home/cloud_frames/frame23.png',
+  '../src/images/home/cloud_frames/frame24.png',
+  // Adicione os caminhos das outras imagens aqui até o frame 24
+];
+
+var animationExecuted = false;
+
+// Função para pré-carregar as imagens
+function preloadImages(callback) {
+  var loadedImages = 0;
+  var totalImages = imagePaths.length;
+
+  function imageLoaded() {
+    loadedImages++;
+    if (loadedImages === totalImages) {
+      callback();
+    }
+  }
+
+  for (var i = 0; i < totalImages; i++) {
+    var img = new Image();
+    img.onload = imageLoaded;
+    img.src = imagePaths[i];
+  }
+}
+
+// Função para executar a animação
+function runAnimation() {
+  var animation = document.getElementById('animation');
+  var images = animation.getElementsByTagName('img');
+  var totalFrames = images.length;
+  var currentFrame = 0;
+
+  function showFrame() {
+    // Oculta todos os frames
+    for (var i = 0; i < totalFrames; i++) {
+      images[i].style.opacity = 0;
+    }
+
+    // Exibe o próximo frame
+    images[currentFrame].style.opacity = 1;
+    currentFrame++;
+
+    if (currentFrame < totalFrames) {
+      setTimeout(showFrame, 1000 / 18); // Framerate de 24fps
+    } else {
+      setTimeout(function() {
+        // Define a opacidade de todos os frames como 0 após o fim da animação
+        for (var i = 0; i < totalFrames; i++) {
+          images[i].style.opacity = 0;
+        }
+      }, 20); // Tempo para manter os frames visíveis após o fim da animação (1 segundo neste caso)
+    }
+  }
+
+  showFrame();
+  animationExecuted = true;
+}
+
+// Carregar as imagens antes de executar a animação
+window.onload = function() {
+};
+
+document.addEventListener('scroll', () => {
+  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+  if (!animationExecuted && window.scrollY >= scrollableHeight * 0.7 && window.scrollY <= scrollableHeight * 0.8) {
+    preloadImages(runAnimation);
+  }
+});
+
